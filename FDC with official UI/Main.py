ad323353f31f6e8ceebf33f5ff77e7b547949de8
FeathersRe <112640075+FeathersRe@users.noreply.github.com>
2023-06-25 21:44:04 +0800

    Add files via upload

--- FDC with official UI/Main.py
+++ FDC with official UI/Main.py
@@ -3,7 +3,7 @@ from ttkbootstrap.constants import *
 from tkinter.filedialog import askopenfilename
 
 
-class Start(ttk.Frame):
+class start(ttk.Frame):
     def __init__(self, master):
         super().__init__(master, padding = (20,10))
         self.filename = ttk.StringVar()
@@ -22,15 +22,15 @@ class Start(ttk.Frame):
 
     def create_mode(self, input_variable):
         lbl_text = "Select your desired conversion mode!"
-        lbl_menu = ["First Degree Conversion(FDC1)", "Final Degree Conversion（FDC2)"]
+        lbl_menu = ["First Degree Conversion(FDC1)", "Final Degree Conversion(FDC2)"]
 
         section = ttk.Frame(self)
         section.pack(fill=X, expand = YES)
         
         lbl = ttk.Label(master=section, font="-size 10", text = lbl_text, anchor = CENTER, width = 30)
-        lbl.pack(fill = X, expand=YES)
+        lbl.pack(fill = X, expand=YES, pady = 10)
 
-        mode = ttk.OptionMenu(section, input_variable, *lbl_menu) #12312321321
+        mode = ttk.OptionMenu(section, input_variable, "", *lbl_menu) 
         mode.pack(fill=X, expand=YES) 
 
     def create_filebrowse(self, input_variable):
@@ -40,7 +40,7 @@ class Start(ttk.Frame):
         section.pack(fill = X, expand = YES)
         
         lbl = ttk.Label(master=section, font="-size 10", text = lbl_text, anchor = CENTER, width = 30)
-        lbl.pack(fill = X, expand=YES)
+        lbl.pack(fill = X, expand=YES, pady = 10)
 
         image_input = ttk.Entry(master=section, textvariable = input_variable)
         image_input.pack(side=LEFT, fill=X, expand = YES, padx=(0, 5), pady=10)
@@ -48,23 +48,34 @@ class Start(ttk.Frame):
         browse_button = ttk.Button(master=section, text="Browse", command = self.search_file)
         browse_button.pack(side=RIGHT, fill=X, padx = (5,0), pady = 10)
 
+    def create_action(self):
+        section = ttk.Frame(self)
+        section.pack(fill=X, expand=YES, pady=(15, 10))
+
+        cnl_btn = ttk.Button(master=section, text="Cancel", command=self.cancel, bootstyle=DANGER, width = 6, )
+        cnl_btn.pack(side=RIGHT, padx = 5)
+
+        conf_btn = ttk.Button(master = section, text = "Submit", command  = self.confirm, bootstyle=SUCCESS, width=6,)
+        conf_btn.pack(side=RIGHT, padx = 5)
+
+    
     def search_file(self):
         path = askopenfilename()
         if path:
             self.filename.set(path)
-            print(self.filename.get())
 
+    def cancel(self):
+        self.quit()
 
-
-
-
-
-    def create_action(self):
-        pass
-
-
+    def confirm(self):
+        selected_mode = self.mode.get()
+        if selected_mode == "First Degree Conversion(FDC1)":
+            img_path = self.filename.get()
+            #first_degree_interface(master, img_path)
+        elif selected_mode == "Final Degree Conversion(FDC2)":
+            ttk.dialogs.dialogs.Messagebox.show_info(message="Function under development. Stay tuned!", title="Final Degree Conversion Module")
 
 if __name__ == "__main__":
     app = ttk.Window("PIXIE 2.0", "litera", resizable = (False, False))
-    Start(app)
+    start(app)
     app.mainloop()
